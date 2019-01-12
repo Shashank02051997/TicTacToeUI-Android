188644b93c41ab1156cb7fe85aa762863743edda
shashank singhal <shashank.singhal@minemarksolutions.com>
2019-01-12 19:25:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# TicTacToeUI-Android
---
+++ app/src/main/java/com/shashank/platform/tictactoeui/MainActivity.java
@@ -0,0 +1,39 @@
+package com.shashank.platform.tictactoeui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button,button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
